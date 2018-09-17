1be477495653ca2ec7b58ce276112dd438598816
pauljamesb <37079324+pauljamesb@users.noreply.github.com>
2018-09-17 11:29:07 +0100

    Update DNCT5Controller.cs

--- Controllers/DNCT5Controller.cs
+++ Controllers/DNCT5Controller.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,9 +9,7 @@ namespace DNCT5.Controllers
 {
     public class DNCT5Controller : Controller
     {
-
         private readonly ApplicationDbContext _db;
-
         public DNCT5Controller(ApplicationDbContext db)
         {
             _db = db;
@@ -20,6 +18,26 @@ namespace DNCT5.Controllers
         public IActionResult Index()
         {
             return View(_db.Items.ToList());
+        }
+
+        // GET Create action
+        public IActionResult Create()
+        {
+          return View();
+        }
+
+        // POST Create Action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Item item)
+        {
+          if(ModelState.IsValid)
+          {
+            _db.Add(item);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+          }
+          return View(item);
         }
     }
 }
